bc4e417bdc50457c76b6742ad6420d962e20538e
Graham Ballantyne <grahamb@sfu.ca>
2014-06-10 21:38:07 -0700

    re-write tests
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- test/program_test.js
+++ test/program_test.js
@@ -1,42 +1,37 @@
 var should = require('should');
-var mongoose = require('mongoose');
+var utils = require('./utils');
 var Program = require('../program');
 var fakeProgarm;
 
-mongoose.connect('mongodb://localhost/pj-models-test');
+
+'use strict';
+
+
 
 describe('ProgramModel', function() {
 
-    beforeEach(function(done) {
-        fakeProgram = {
-            name: 'Discover SCUBA',
-            description: 'Participants will discover SCUBA diving',
-            location: 'offsite',
-            programPeriodsAvailable: 5,
-            maxParticipantsPerPeriod: 48,
-            programPeriodsRequired: 2,
-            fee: 0,
-            isOvernight: false
-        };
-        Program.remove(done);
-    });
+    var fakeProgram = {
+        name: 'Discover SCUBA',
+        description: 'Participants will discover SCUBA diving',
+        location: 'offsite',
+        programPeriodsAvailable: 5,
+        maxParticipantsPerPeriod: 48,
+        programPeriodsRequired: 2,
+        fee: 0,
+        isOvernight: false
+    };
 
-    describe('#save()', function() {
-        var program;
-        beforeEach(function(done) {
-            program = new Program(fakeProgram);
-            done();
-        });
-
-        it('should have a name', function(done) {
-            program.save(function(err, program) {
+    describe('#create()', function() {
+        it('should create a new Program', function(done) {
+            Program.create(fakeProgram, function(err, program) {
                 should.not.exist(err);
-                program.should.have.property('name', 'Discover SCUBA');
+                program.name.should.equal('Discover SCUBA');
                 done();
             });
         });
 
         it('should not save without a name', function(done) {
+            var program = new Program(fakeProgram);
             program.name = '';
             program.save(function(err, program) {
                 should.exist(err);
@@ -46,6 +41,7 @@ describe('ProgramModel', function() {
         });
 
         it('should not save with an invalid location', function(done) {
+            var program = new Program(fakeProgram);
             program.location = 'somewhere else',
             program.save(function(err, program) {
                 should.exist(err);
