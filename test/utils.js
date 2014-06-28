adc85bcb06dcf6028840619b3a812ffef300c324
Graham Ballantyne <grahamb@sfu.ca>
2014-06-28 15:06:12 -0700

    add config file for mongoUrl settings

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var config = require('./config');
 
 // ensure the NODE_ENV is set to 'test'
 // this is helpful when you would like to change behavior when testing
@@ -8,7 +9,7 @@ process.env.NODE_ENV = 'test';
 
 before(function(done) {
     if (mongoose.connection.readyState === 0) {
-        mongoose.connect('mongodb://localhost/pj-models-test', function (err) {
+        mongoose.connect(config.mongoUrl, function (err) {
             if (err) {
                 throw err;
             }
